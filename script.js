2ef4a79eb65aff247666a660e5bbaa711875046b
c20240088.regino-droid <c20240088.regino@csav.edu.ph>
2026-09-11 11:36:56 +0800

    textContent

--- script.js
+++ script.js
@@ -40,7 +40,9 @@ console.log(contactHeading);
 const projectHeading = document.querySelector("#services h2");
 console.log(projectHeading);
 
-
-
+//Text Context
+heading.textContent = "My portfolio";
+contactHeading.textContent = "Let's Connect!";
+projectHeading.textContent = "My Projects";
 
 
