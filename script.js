795cc432f344c884b1a9216f1c0e62dbb7ab8e5b
c20240088.regino-droid <c20240088.regino@csav.edu.ph>
2026-09-11 09:25:41 +0800

    variable (let & const)

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Hello world.");
+const Myname = "John Rey P. Regino";
+console.log('NAME:' + Myname);
+let age = 20;
+console.log('AGE:' + age);
+let number = 92342423;
+console.log('NUMBER:' + number);
+let address = "Brgy 6-a";
+console.log('ADDRESS:' + address);
